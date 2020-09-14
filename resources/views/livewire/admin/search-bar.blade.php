a0f771fc7897ae7b85a85a9b7da7a952479c839f
Loyd Tafireyi <loydtafireyi@gmail.com>
2020-09-14 08:34:03 +0200

    Admin Product Search UI

--- resources/views/livewire/admin/search-bar.blade.php
+++ resources/views/livewire/admin/search-bar.blade.php
@@ -1,27 +1,35 @@
 <div class="col-md-5">
     <input type="text" name="" class="form-control rounded text-center position-relative" placeholder="Search Products">
     <ul class="list-group position-absolute mt-2 ml-1 col-md-11">
-    	<li class="list-group-item  bg-primary">
-    		<a href="" class="d-flex justify-content-between">
-    			<span class="text-light mt-4 text-capitalize"><h6>Cakes</h6></span>
+    	<li class="list-group-item  bg-primary border-bottom">
+    		<a href="" class="d-flex justify-content-between  text-decoration-none">
+    			<span class="text-light mt-4 text-capitalize">
+    				<h6 class="font-weight-bold" style="letter-spacing: 2px">Cakes</h6>
+    			</span>
     			<img src="{{ asset('frontend/img/blog-thumbs/1.jpg') }}" style="width: 50px; height: 50px; border-radius: 100%;">
     		</a>
     	</li>
-    	<li class="list-group-item  bg-primary">
-    		<a href="" class="d-flex justify-content-between">
-    			<span class="text-light mt-4 text-capitalize"><h6>Cakes</h6></span>
+    	<li class="list-group-item  bg-primary border-bottom">
+    		<a href="" class="d-flex justify-content-between  text-decoration-none">
+    			<span class="text-light mt-4 text-capitalize">
+    				<h6 class="font-weight-bold" style="letter-spacing: 2px">Cakes</h6>
+    			</span>
     			<img src="{{ asset('frontend/img/blog-thumbs/1.jpg') }}" style="width: 50px; height: 50px; border-radius: 100%;">
     		</a>
     	</li>
-    	<li class="list-group-item  bg-primary">
-    		<a href="" class="d-flex justify-content-between">
-    			<span class="text-light mt-4 text-capitalize"><h6>Cakes</h6></span>
+    	<li class="list-group-item  bg-primary border-bottom">
+    		<a href="" class="d-flex justify-content-between  text-decoration-none">
+    			<span class="text-light mt-4 text-capitalize">
+    				<h6 class="font-weight-bold" style="letter-spacing: 2px">Cakes</h6>
+    			</span>
     			<img src="{{ asset('frontend/img/blog-thumbs/1.jpg') }}" style="width: 50px; height: 50px; border-radius: 100%;">
     		</a>
     	</li>
-    	<li class="list-group-item  bg-primary">
-    		<a href="" class="d-flex justify-content-between">
-    			<span class="text-light mt-4 text-capitalize"><h6>Cakes</h6></span>
+    	<li class="list-group-item  bg-primary border-bottom">
+    		<a href="" class="d-flex justify-content-between  text-decoration-none">
+    			<span class="text-light mt-4 text-capitalize">
+    				<h6 class="font-weight-bold" style="letter-spacing: 2px">Cakes</h6>
+    			</span>
     			<img src="{{ asset('frontend/img/blog-thumbs/1.jpg') }}" style="width: 50px; height: 50px; border-radius: 100%;">
     		</a>
     	</li>
